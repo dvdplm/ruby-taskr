5a699dcf8d63647116b56880b8f091bb819f990c
matt.zukowski <matt.zukowski@dda205c7-5e3f-0410-a417-dfc30505af65>
2007-12-03 16:23:18 +0000

    added ruby example
    git-svn-id: http://ruby-taskr.googlecode.com/svn/trunk@59 dda205c7-5e3f-0410-a417-dfc30505af65

---
+++ examples/active_resource_client_example.rb
@@ -0,0 +1,36 @@
+require 'active_resource'
+
+# Define the proxy class
+class Task < ActiveResource::Base
+  self.site = 'http://taskr.example.com'
+end
+
+# Retrieve a list of all the Tasks
+tasks = Task.find(:all)
+
+# Print each Task's details for debugging
+tasks.each {|t| puts t.inspect}
+
+# Retrieve a specific Task
+task = Task.find(123)
+
+# Print the Task's name
+puts task.name
+
+# Create a new Task to be executed every 10 seconds
+task = Task.new
+task.name = "My Example Task"
+task.schedule_method = 'every'
+task.schedule_when = '10s'
+task.actions = [
+    {:action_class_name => 'Ruby', :code => "puts 'Hello World!'"},
+    {:action_class_name => 'Ruby', :code => "puts 'Goodbye!'"}
+  ]
+  
+# Save the new Task -- the Task is not scheduled for execution until
+# it is saved.
+task.save
+
+# Deleting a task
+id = 123
+Task.delete(id)
